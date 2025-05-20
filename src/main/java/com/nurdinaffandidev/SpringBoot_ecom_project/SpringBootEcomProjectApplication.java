3154dd9d06b9910a832c5bb404ed4d6f2653983d
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-05-20 15:54:03 +0800

    intital commit: intialize spring boot

---
+++ src/main/java/com/nurdinaffandidev/SpringBoot_ecom_project/SpringBootEcomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nurdinaffandidev.SpringBoot_ecom_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcomProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nurdinaffandidev/SpringBoot_ecom_project/SpringBootEcomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurdinaffandidev.SpringBoot_ecom_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
