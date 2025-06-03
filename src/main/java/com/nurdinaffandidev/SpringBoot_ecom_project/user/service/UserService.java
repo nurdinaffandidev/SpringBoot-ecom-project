d5a36ade0c10c266b390b8703964291ebef004d9
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-03 15:38:01 +0800

    feat[user]: implement jwt service for generating jwt, create verify function for login authentication

--- src/main/java/com/nurdinaffandidev/SpringBoot_ecom_project/user/service/UserService.java
+++ src/main/java/com/nurdinaffandidev/SpringBoot_ecom_project/user/service/UserService.java
@@ -4,6 +4,9 @@ import com.nurdinaffandidev.SpringBoot_ecom_project.user.model.User;
 import com.nurdinaffandidev.SpringBoot_ecom_project.user.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,12 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JWTService jwtService;
 
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
@@ -28,5 +37,13 @@ public class UserService {
         }
         newUser.setPassword(encoder.encode(newUser.getPassword()));
         return userRepository.save(newUser);
+    }
+
+    public String verify(User user) {
+        Authentication authentication =  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(user.getUsername());
+        }
+        return "Login Failed";
     }
 }
